228752b756887bc86985d9cb538f27229d54983f
Chris Paldino <cpaldino9@gmail.com>
2020-05-16 19:03:23 -0400

    navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,34 +1,33 @@
 import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import { AuthContext } from '../firebase/Auth';
-import SignOutButton from './SignOut';
 import '../App.css';
 
 const Navigation = () => {
 	const { currentUser } = useContext(AuthContext);
 
 	const nav=
-		<nav className="navigation navbar navbar-expand-lg navbar-light bg-dark">
+	<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 		<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-		<span className="navbar-toggler-icon"></span>
+			<span className="navbar-toggler-icon"></span>
 		</button>
 		<div className="collapse navbar-collapse" id="navbarNav">
-		<ul className="navbar-nav">
-		<li className="nav-item">
-		<NavLink exact to="/home" activeClassName="active">
-		Home
-		</NavLink>
+			<ul className="navbar-nav">
+				<li className="nav-item">
+					<NavLink className="nav-link" exact to="/home" activeClassName="active">
+					Home
+				</NavLink>
+			</li>
+			<li className="nav-item">
+				<NavLink className="nav-link" exact to="/account" activeClassName="active">
+				Account
+			</NavLink>
 		</li>
-		<li className="nav-item">
-		<NavLink exact to="/account" activeClassName="active">
-		Account
-		</NavLink>
-		</li>
-		</ul>
-		</div>
-		</nav>;
+	</ul>
+</div>
+</nav>;
 
-	return (currentUser?nav:null);
+return (currentUser?nav:null);
 
 };
 
