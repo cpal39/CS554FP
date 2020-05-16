540d9298b8d268554fd6c0bacd6e8d943d18f600
Chris Paldino <cpaldino9@gmail.com>
2020-05-16 14:10:22 -0400

    Update Landing.js

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,36 +5,32 @@ import SignIn from './SignIn';
 function Landing() {
 	const [signUp, setSignUp] = useState(true);
 	const handleClick = () => {signUp? setSignUp(false):setSignUp(true)};
-	const landUp=
-		<div id="landingParent">
-			<div id="landingChild">
-				<h2>COVID-19 Tracker</h2>
-				<h3>Developed by Hien Bui, Samantha DeLorenzo, and Chris Paldino</h3>
-				<SignUp/>
-				Already have an account? &nbsp;
-				<button className="btn btn-link" onClick={handleClick}>
-						Sign in
-				</button>
-			</div>
-		</div>;
-
-	const landIn=
+	return (
 		<div id="landingParent">
 			<div id="landingChild">
 				<div id="landingHeader">
 					<h2>COVID-19 Tracker</h2>
 					<h3>Developed by Hien Bui, Samantha DeLorenzo, and Chris Paldino</h3>
 				</div>
-				<SignIn/>
-				Don't have an account? &nbsp;
-				<button className="btn btn-link" onClick={handleClick}>
-						Sign up
-				</button>
+				{signUp?
+					<div id="landingContent">
+						<SignUp/>
+						Already have an account? &nbsp;
+						<button className="btn btn-link" onClick={handleClick}>
+								Sign in
+						</button>
+					</div>
+					:<div id="landingContent">
+						<SignIn/>
+						Don't have an account? &nbsp;
+						<button className="btn btn-link" onClick={handleClick}>
+								Sign up
+						</button>
+					</div>
+				}
 			</div>
-		</div>;
-
-	return (signUp? landUp: landIn);
-
+		</div>
+	);
 }
 
 export default Landing;
